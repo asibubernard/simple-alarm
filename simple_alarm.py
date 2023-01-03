6c2d7245df263a9ae1c675a1cd0a2de0972d5770
Bernard Asibu <44650300+asibubernard@users.noreply.github.com>
2023-01-03 18:33:13 +0000

    initial commit

---
+++ simple_alarm.py
@@ -0,0 +1,32 @@
+import argparse
+import datetime
+import pytz
+import time
+from playsound import playsound
+
+
+def set_alarm(hour, minute):
+    # Get the current time
+    current_time = datetime.datetime.now(pytz.timezone('UTC'))
+    # Set the alarm time
+    alarm_time = current_time.replace(hour=hour, minute=minute, second=0, microsecond=0)
+    # Check if the alarm time is in the future
+    if alarm_time > current_time:
+        # Sleep for the amount of time until the alarm should go off
+        time.sleep((alarm_time - current_time).total_seconds())
+    # Print a message to indicate that the alarm has gone off
+    print("Time up!")
+    playsound("alarm_sound.mp3")
+
+if __name__ == "__main__":
+    # Create an argument parser
+    parser = argparse.ArgumentParser()
+    # Add arguments for the alarm time
+    parser.add_argument('--hour', type=int, required=True, help='The hour at which the alarm should go off')
+    parser.add_argument('--minute', type=int, required=True, help='The minute at which the alarm should go off')
+    # Parse the command line arguments
+    args = parser.parse_args()
+    # Set the alarm using the arguments provided
+    set_alarm(args.hour, args.minute)
+
+
